1bbcb55727a66fc3d61de28d594ea7597064e7bf
MercyKorir <mcthegreat78@gmail.com>
2023-08-16 15:08:42 +0300

    I have added the file for task 0*

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,23 +2,32 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = 'https://swapi-api.alx-tools.com/api/';
 
-request.get(url, (err, res, body) => {
-  if (!err && res.statusCode === 200) {
-    const data = JSON.parse(body);
-    const chars = data.characters;
-    chars.forEach(char => {
-      request.get(char, (err, res, body) => {
-        if (!err && res.statusCode === 200) {
-          const charData = JSON.parse(body);
-          console.log(charData.name);
-        } else {
-          console.error('An error occured: ', err);
-        }
-      });
-    });
-  } else {
-    console.error('An error occured: ', err);
-  }
-});
+function printName (charUrl) {
+  request.get(charUrl, (err, res, body) => {
+    if (!err && res.statusCode === 200) {
+      const data = JSON.parse(body);
+      console.log(data.name);
+    } else {
+      console.error('An error occured: ', err);
+    }
+  });
+}
+
+function printMovie (movieId) {
+  const movieUrl = `${url}films/${movieId}/`;
+  request.get(movieUrl, (err, res, body) => {
+    if (!err && res.statusCode === 200) {
+      const data = JSON.parse(body);
+      const charList = data.characters;
+      for (const char of charList) {
+        printName(char);
+      }
+    } else {
+      console.error('An error occured: ', err);
+    }
+  });
+}
+
+printMovie(movieId);

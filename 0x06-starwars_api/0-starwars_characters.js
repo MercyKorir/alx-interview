99546f921ecd0f54c15650f0c354dc425c864467
MercyKorir <mcthegreat78@gmail.com>
2023-08-16 15:06:31 +0300

    I have added the file for task 0*

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (err, res, body) => {
+  if (!err && res.statusCode === 200) {
+    const data = JSON.parse(body);
+    const chars = data.characters;
+    chars.forEach(char => {
+      request.get(char, (err, res, body) => {
+        if (!err && res.statusCode === 200) {
+          const charData = JSON.parse(body);
+          console.log(charData.name);
+        } else {
+          console.error('An error occured: ', err);
+        }
+      });
+    });
+  } else {
+    console.error('An error occured: ', err);
+  }
+});

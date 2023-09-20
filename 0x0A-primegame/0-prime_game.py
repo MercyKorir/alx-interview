c85d15aafcc0f8df2307cd2c1c276f6371d0bd35
MercyKorir <mcthegreat78@gmail.com>
2023-09-20 08:15:17 +0300

    I have added the file for task 0*

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Definition of function isWinner"""
+
+
+def isWinner(x, nums):
+    """determines who winner of each game is"""
+    def calculate_primes(n):
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False
+        p = 2
+        while p * p <= n:
+            if primes[p]:
+                for i in range(p * p, n + 1, p):
+                    primes[i] = False
+            p += 1
+        return [i for i in range(2, n + 1) if primes[i]]
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        primes = calculate_primes(n)
+        remaining_numbers = list(range(1, n + 1))
+        maria_turn = True
+
+        while primes and remaining_numbers:
+            chosen_prime = min(primes)
+            primes.remove(chosen_prime)
+            for multiple in range(chosen_prime, n + 1, chosen_prime):
+                if multiple in remaining_numbers:
+                    remaining_numbers.remove(multiple)
+
+            maria_turn = not maria_turn
+
+        if maria_turn:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None

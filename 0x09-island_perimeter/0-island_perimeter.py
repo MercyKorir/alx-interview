afbfddc5a9f49938faac9c60c82950cd44591581
MercyKorir <mcthegreat78@gmail.com>
2023-09-20 07:59:09 +0300

    I have added the file for task 0*

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Definition of function island_perimeter"""
+
+def island_perimeter(grid):
+    """returns perimeter of island described"""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter

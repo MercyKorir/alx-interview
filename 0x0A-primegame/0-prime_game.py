1c3f670b0d17c417d7548c6124dd54941392db1d
MercyKorir <mcthegreat78@gmail.com>
2023-09-29 09:30:13 +0300

    fix: prime file error

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,44 +2,41 @@
 """Definition of function isWinner"""
 
 
+def is_prime(num):
+    """determine if a number is prime"""
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def get_primes(n):
+    """gets all prime numbers"""
+    primes = []
+    for num in range(2, n + 1):
+        if is_prime(num):
+            primes.append(num)
+    return primes
+
+
 def isWinner(x, nums):
     """determines who winner of each game is"""
-    def calculate_primes(n):
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-        curr_prime = 2
-        while curr_prime * curr_prime <= n:
-            if primes[curr_prime]:
-                for i in range(curr_prime * curr_prime, n + 1, curr_prime):
-                    primes[i] = False
-            curr_prime += 1
-        return [i for i in range(2, n + 1) if primes[i]]
-
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        primes = calculate_primes(n)
-        remaining_numbers = list(range(1, n + 1))
-        maria_turn = True
+        primes = get_primes(n)
+        num_primes = len(primes)
 
-        while primes and remaining_numbers:
-            chosen_prime = min(primes)
-            primes.remove(chosen_prime)
-            for multiple in range(chosen_prime, n + 1, chosen_prime):
-                if multiple in remaining_numbers:
-                    remaining_numbers.remove(multiple)
-
-            maria_turn = not maria_turn
-
-        if maria_turn:
+        if num_primes % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+        if maria_wins > ben_wins:
+            return "Maria"
+        elif ben_wins > maria_wins:
+            return "Ben"
+        else:
+            return None

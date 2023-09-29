1506651a6cbc1bad194832ad7dcb34bac5ae5366
MercyKorir <mcthegreat78@gmail.com>
2023-09-29 09:41:06 +0300

    fix: prime file error

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -34,9 +34,9 @@ def isWinner(x, nums):
             ben_wins += 1
         else:
             maria_wins += 1
-        if maria_wins > ben_wins:
-            return "Maria"
-        elif ben_wins > maria_wins:
-            return "Ben"
-        else:
-            return None
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None

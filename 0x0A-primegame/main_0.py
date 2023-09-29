d5b99b51449bd6346afe6751fad1121843c5ab30
MercyKorir <mcthegreat78@gmail.com>
2023-09-29 12:30:58 +0300

    fix: prime file error

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""
-Main file for testing
-"""
 
 isWinner = __import__('0-prime_game').isWinner
 
 
-print("Winner: {}".format(isWinner(-1, [10])))
+print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
